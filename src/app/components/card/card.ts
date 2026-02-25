a8a9b89c8bb4bdd94d66736593170626000a3828
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-02-25 23:37:41 +0100

    implementar la home

--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Users } from '../../services/users';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './card.html',
   styleUrl: './card.css',
 })
 export class Card {
+  miUsuario:any;
+  usersService=inject(Users);
 
+  @Input() imagen:string='';
+  @Input() nombre:string='';
+  @Input() id:number=0;
+  @Input() _id:string|undefined='';
+
+  eliminar($event:any,id:any){
+      $event.preventDefault();
+      Swal.fire({
+        title: '¿Estás seguro de eliminar el usuario?',
+        showDenyButton: false,
+        showCancelButton: true,
+        confirmButtonText: 'Confirmar',
+        confirmButtonColor: '#ff0000',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            let eliminar:any;
+  
+            eliminar=this.usersService.deleteUser(id).subscribe((data)=>{
+            console.log(data);
+  
+            if(data.error){
+              Swal.fire('Ha habido un error', '', 'info');
+            }else{
+              Swal.fire('Eliminado!', data.first_name, 'success');
+            }
+          });
+        }
+      });
+    }
 }
--- src/app/components/main/main.ts
+++ src/app/components/main/main.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Users } from '../../services/users';
+import { CommonModule } from '@angular/common';
+import { map } from 'rxjs';
+import { Card } from '../card/card';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [CommonModule,Card],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
 export class Main {
+  usersService = inject(Users);
 
+  items$ = this.usersService.getAllUsers().pipe(
+    map((resp) => resp.results)
+  );
 }
