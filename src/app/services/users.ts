bc3fadbf28ec847c0f729ece56a4e7c51ac10768
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-02-25 22:44:39 +0100

    implementar servicio users

--- src/app/services/users.ts
+++ src/app/services/users.ts
@@ -1,8 +1,38 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IUserResponse } from '../interfaces/i-user-response';
+import { firstValueFrom, Observable } from 'rxjs';
+import { IUser } from '../interfaces/i-user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Users {
-  
+  private baseUrl='https://peticiones.online/api/users';
+  httpClient=inject(HttpClient)
+
+  getAllUsers():Observable<IUserResponse>{
+    return this.httpClient.get<IUserResponse>(this.baseUrl);
+  }
+
+  getAllUsersPromise():Promise<IUserResponse>{
+    return firstValueFrom(this.httpClient.get<IUserResponse>(this.baseUrl));
+  }
+
+  getUserById(id:string):Observable<IUser>{
+    return this.httpClient.get<IUser>(this.baseUrl+'/'+id);
+  }
+
+  insertUser(body:IUser):Observable<any>{
+    let url='https://peticiones.online/api/users';
+    return this.httpClient.post(url,body, {});
+  }
+  deleteUser(id:string):Observable<any>{
+    let url='https://peticiones.online/api/users/';
+    return this.httpClient.delete(url + id,{});
+  }
+  updateUser(id:number,body:IUser):Observable<any>{
+    let url='https://peticiones.online/api/users/';
+    return this.httpClient.put(url + id,body,{})
+  }
 }
