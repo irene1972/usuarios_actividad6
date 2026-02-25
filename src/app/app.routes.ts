5d019150f26f1362ec26928d4ee2e20b7da3f71c
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-02-25 22:08:57 +0100

    crear las rutas del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Main } from './components/main/main';
+import { Formulario } from './components/formulario/formulario';
+import { Detalle } from './components/detalle/detalle';
+import { C404 } from './components/c404/c404';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',pathMatch:'full',redirectTo:'home'},
+    {path:'home',component:Main},
+    {path:'formulario',component:Formulario},
+    {path:'user/:id',component:Detalle},
+    {path:'updateuser/:id',component:Formulario},
+    {path:'newuser',component:Formulario},
+    {path:'**',component:C404}
+];
