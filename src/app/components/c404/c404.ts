026899e3521fa9763932cf16d37c4f351332fbcf
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-02-25 22:04:54 +0100

    crear componentes necesarios

---
+++ src/app/components/c404/c404.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-c404',
+  imports: [],
+  templateUrl: './c404.html',
+  styleUrl: './c404.css',
+})
+export class C404 {
+
+}
